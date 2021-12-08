25deaec7cfb08b72c587c468851709f708ab635b
test <cordial14@hanmail.net>
2021-12-08 23:14:22 +0900

    네번재 커밋 오류있음

--- models/User.js
+++ models/User.js
@@ -81,7 +81,7 @@ userSchema.methods.generateToken = function(cb){
 userSchema.statics.findByToken = function (token, cb){
     var user = this;
     //토큰을 decode한다.
-    jwt.verify(token, 'secretToken', function(err, decode){
+    jwt.verify(token, 'secretToken', function(err, decoded){
         // 유저 아이디를 이용해서 유저를 찾은 다음에
         // 클라이언트에서 가져온 토큰과 db에 보관된 토큰이 일치하는지 확인
 
